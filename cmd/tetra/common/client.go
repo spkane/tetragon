846b614779cd473864b8bf92a2f4d0f12a04dedb
Mahe Tardy <mahe.tardy@gmail.com>
2024-07-16 10:36:30 +0200

    tetra: switch CliRun to use NewClient instead of DialContext
    DialContext got deprecated, so we switch to NewClient that does not
    connect to the server, it waits for the first RPC to be called to
    establish a connection, thus the retry mechanism at connect no longer
    make sense. It could be reimplemented as a retry helper for the RPC
    call.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- cmd/tetra/common/client.go
+++ cmd/tetra/common/client.go
@@ -8,57 +8,20 @@ import (
 	"fmt"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/cilium/tetragon/api/v1/tetragon"
-	"github.com/cilium/tetragon/pkg/logger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func connect(ctx context.Context) (*grpc.ClientConn, string, error) {
-	connCtx, connCancel := context.WithTimeout(ctx, Timeout)
-	defer connCancel()
-
-	address, err := ResolveServerAddress()
-	if err != nil {
-		return nil, "", fmt.Errorf("failed to resolve server address: %w", err)
-	}
-
-	conn, err := grpc.DialContext(connCtx, address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-	return conn, address, err
-}
-
 func CliRunErr(fn func(ctx context.Context, cli tetragon.FineGuidanceSensorsClient), fnErr func(err error)) {
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
-
-	var conn *grpc.ClientConn
-	var serverAddr string
-	var err error
-
-	backoff := time.Second
-	attempts := 0
-	for {
-		conn, serverAddr, err = connect(ctx)
-		if err != nil {
-			if attempts < Retries {
-				// Exponential backoff
-				attempts++
-				logger.GetLogger().WithField("server-address", serverAddr).WithField("attempts", attempts).WithError(err).Error("Connection attempt failed, retrying...")
-				time.Sleep(backoff)
-				backoff *= 2
-				continue
-			}
-			logger.GetLogger().WithField("server-address", serverAddr).WithField("attempts", attempts).WithError(err).Fatal("Failed to connect to server")
-			fnErr(err)
-		}
-		break
+	c, err := NewClientWithDefaultContext()
+	if err != nil {
+		fnErr(err)
+		return
 	}
-	defer conn.Close()
-
-	client := tetragon.NewFineGuidanceSensorsClient(conn)
-	fn(ctx, client)
+	defer c.Close()
+	fn(c.Ctx, c.Client)
 }
 
 func CliRun(fn func(ctx context.Context, cli tetragon.FineGuidanceSensorsClient)) {
